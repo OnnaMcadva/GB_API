1a2728cf0624bb30860b90e4759c1e8cedafc1be
Onna Mcadva <anna.anutka@gmail.com>
2023-07-29 17:19:46 +0200

    Восстановите выражение

---
+++ src/main/java/org/example/lesson1/Printer4.java
@@ -0,0 +1,20 @@
+package org.example.lesson1;
+
+
+public class Printer4{
+    public static void main(String[] args) {
+        String file = "";
+
+        if (args.length == 0) {
+            file = "input.txt";
+        }
+        else{
+            file = args[0];
+        }
+
+        Equation eq = new Equation();
+        String result = eq.getSolution(file);
+        System.out.println(result);
+    }
+}
+
